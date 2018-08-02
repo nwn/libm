2d1554fbea55b4a897a62005afa72bc31f603811
Nathan Wiebe Neufeldt <wn.nathan@gmail.com>
2018-08-02 02:16:54 -0400

    initial port

--- src/math/hypotf.rs
+++ src/math/hypotf.rs
@@ -1,43 +1,81 @@
-use core::f32;
-
 use super::sqrtf;
 
 #[inline]
-pub fn hypotf(mut x: f32, mut y: f32) -> f32 {
-    let x1p90 = f32::from_bits(0x6c800000); // 0x1p90f === 2 ^ 90
-    let x1p_90 = f32::from_bits(0x12800000); // 0x1p-90f === 2 ^ -90
+pub fn hypotf(x: f32, y: f32) -> f32 {
+    // Arrange |x| >= |y|
+    let hx = x.to_bits() as i32 & 0x7fffffff;
+    let hy = y.to_bits() as i32 & 0x7fffffff;
+    let (mut hx, mut hy) = if hx < hy { (hy, hx) } else { (hx, hy) };
+    let mut x = f32::from_bits(hx as u32);
+    let mut y = f32::from_bits(hy as u32);
 
-    let mut uxi = x.to_bits();
-    let mut uyi = y.to_bits();
-    let uti;
-    let mut z: f32;
-
-    uxi &= -1i32 as u32 >> 1;
-    uyi &= -1i32 as u32 >> 1;
-    if uxi < uyi {
-        uti = uxi;
-        uxi = uyi;
-        uyi = uti;
-    }
-
-    x = f32::from_bits(uxi);
-    y = f32::from_bits(uyi);
-    if uyi == 0xff << 23 {
-        return y;
-    }
-    if uxi >= 0xff << 23 || uyi == 0 || uxi - uyi >= 25 << 23 {
+    // x / y > 2**30
+    if hx - hy > 0xf000000i32 {
         return x + y;
     }
 
-    z = 1.;
-    if uxi >= (0x7f + 60) << 23 {
-        z = x1p90;
-        x *= x1p_90;
-        y *= x1p_90;
-    } else if uyi < (0x7f - 60) << 23 {
-        z = x1p_90;
-        x *= x1p90;
-        y *= x1p90;
+    let mut exp = 0i32;
+
+    // x > 2**50
+    if hx > 0x58800000i32 {
+        // Inf or NaN
+        if hx >= 0x7f800000i32 {
+            return if hx == 0x7f800000 {
+                x
+            } else if hy == 0x7f800000 {
+                y
+            } else {
+                x + y // for sNaN
+            };
+        }
+
+        // Scale x and y by 2**-60
+        hx -= 0x5d800000;
+        hy -= 0x5d800000;
+        x = f32::from_bits(hx as u32);
+        y = f32::from_bits(hy as u32);
+        exp += 60;
     }
-    z * sqrtf((x as f64 * x as f64 + y as f64 * y as f64) as f32)
+
+    // y < 2**-50
+    if hy < 0x26800000i32 {
+        // 0 or subnormal
+        if hy <= 0x007fffffi32 {
+            if hy == 0 {
+                return x;
+            }
+
+            let t1 = f32::from_bits(0x3f000000); // 2**126
+            x *= t1;
+            y *= t1;
+            exp -= 126;
+        } else {
+            // Scale x and y by 2**60
+            hx += 0x5d800000;
+            hy += 0x5d800000;
+            x = f32::from_bits(hx as u32);
+            y = f32::from_bits(hy as u32);
+            exp -= 60;
+        }
+    }
+
+    // Medium sized x and y
+    let mut w = x - y;
+    if w > y {
+        let t1 = f32::from_bits(hx as u32 & 0xfffff000);
+        let t2 = x - t1;
+        w = sqrtf(t1 * t1 - ((y * -y) - t2 * (x + t1)));
+    } else {
+        x += x;
+        let y1 = f32::from_bits(hy as u32 & 0xfffff000);
+        let y2 = y - y1;
+        let t1 = f32::from_bits(hx as u32 + 0x00800000);
+        let t2 = x - t1;
+        w = sqrtf(t1 * y1 - ((w * -w) - (t1 * y2 + t2 * y)));
+    }
+    if exp != 0 {
+        w *= f32::from_bits(0x3f800000u32.wrapping_add((exp as u32) << 23));
+    }
+
+    w
 }
